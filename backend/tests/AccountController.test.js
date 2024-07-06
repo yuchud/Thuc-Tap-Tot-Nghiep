4a5d5c96373efb8899c6863eec6a69a0635d0d14
yuchud <studywithhuy2002@gmail.com>
2024-07-07 03:24:55 +0700

    try fix Jest
    add before All and change url test

--- backend/tests/AccountController.test.js
+++ backend/tests/AccountController.test.js
@@ -1,17 +1,24 @@
 const supertest = require('supertest');
-const app = require('../app'); // Adjust the path as necessary
+const app = require('../app');
 
 
 describe('Account Controller Tests', () => {
-  afterAll(done => {
-    app.server.close(done);
-  });
-  
+
+  beforeAll(() => {
+    server = app.listen(8765); // Start the server before the tests
+ })
+
+    
   test('Should respond to a GET request', async () => {
-    await supertest(app).get('/accounts')
+    await supertest(app).get('/accounts/id/1')
       .expect(200) // Adjust expectations as necessary
       .then((response) => {
         // Perform assertions on the response
       });
-  });
+  }); 
+
+  
+  afterAll(done => {
+    app.server.close(done);
+  }); // Close the server after the tests
 });
