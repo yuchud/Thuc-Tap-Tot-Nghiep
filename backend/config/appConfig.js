194d72016dbf6cc33c12ca36f6fe9775ba0ebb64
yuchud <studywithhuy2002@gmail.com>
2024-07-07 02:58:24 +0700

    add db port

--- backend/config/appConfig.js
+++ backend/config/appConfig.js
@@ -1,13 +1,7 @@
 module.exports = {
     PORT: 3000, // Make sure this port is different from React app's port
     USER_ROLES: {
-        ADMIN: {
-            id: 1,
-            name: 'admin',
-        },
-        CUSTOMER: {
-            id: 2,
-            name: 'customer',
-        },
+        ADMIN: 1,
+        CUSTOMER: 2
     }
 };
