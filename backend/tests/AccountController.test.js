584550636e77c9d83765343ce96b7355dee1a65e
yuchud <studywithhuy2002@gmail.com>
2024-07-07 02:42:10 +0700

    try Jest to test project

---
+++ backend/tests/AccountController.test.js
@@ -0,0 +1,12 @@
+const supertest = require('supertest');
+const app = require('../app'); // Adjust the path as necessary
+
+describe('Account Controller Tests', () => {
+  test('Should respond to a GET request', async () => {
+    await supertest(app).get('/accounts')
+      .expect(200) // Adjust expectations as necessary
+      .then((response) => {
+        // Perform assertions on the response
+      });
+  });
+});
