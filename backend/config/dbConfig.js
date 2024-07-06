24836327ad14a709275e4f57cc4a98dc1427d4e3
yuchud <studywithhuy2002@gmail.com>
2024-07-06 22:27:02 +0700

    try remove dialect in dbconfig.js

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -3,7 +3,7 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'mysql',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
