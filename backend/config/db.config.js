a496de95405e866c9d78688f697cf36c2a77444b
yuchud <studywithhuy2002@gmail.com>
2024-07-07 08:59:49 +0700

    Test Ci
    change host

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    host: process.env.DB_HOST,
+    //host: process.env.DB_HOST,
+    host: '172.18.0.1',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
